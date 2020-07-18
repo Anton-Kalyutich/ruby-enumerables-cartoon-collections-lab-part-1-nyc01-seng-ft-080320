ed6a6af3c878556d75ba2b78051510e8108e8642
Anton Kalyutich <anton.kalyutich@gmail.com>
2020-07-18 20:46:07 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -12,7 +12,6 @@ def list_dwarves(array)
   #
   # Print a numbered list of each element
   array.each_with_index do |element, index|
-    array[index] = "#{index}. #{element}"
+    puts "#{index}. #{element}"
   end
-  array
 end
